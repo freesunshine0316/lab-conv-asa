4eba3a366fccda7b66b948fe0da39f21bd503aa4
lfsong <freesunshine0316@gmail.com>
2022-10-01 22:58:34 -0700

    initial code for generating data

--- asa_datastream.py
+++ asa_datastream.py
@@ -1,29 +1,45 @@
-
-import os, sys, json, codecs
+import os, sys, copy, json
 import numpy as np
 import torch
-import ast
-import collections
 
-SENTI_STR_MAPPING = {1:'Pos', 0:'Neu', -1:'Neg'}
-TAG_MAPPING = {'O':0, 'PosB':1, 'PosI':2, 'NeuB':3, 'NeuI':4, 'NegB':5, 'NegI':6}
+from transformers import BertTokenizer
+
+SENTI_STR_MAPPING = {1: 'Pos', 0: 'Neu', -1: 'Neg'}
+TAG_MAPPING = {'O': 0, 'PosB': 1, 'PosI': 2, 'NeuB': 3, 'NeuI': 4, 'NegB': 5, 'NegI': 6}
 TAGS = ['O', 'PosB', 'PosI', 'NeuB', 'NeuI', 'NegB', 'NegI']
 
 
 def is_tag_begin(tid):
-    return tid in (1,3,5,)
+    return tid in (
+        1,
+        3,
+        5,
+    )
 
 
 def is_tag_inner(tid):
-    return tid in (2,4,6,)
+    return tid in (
+        2,
+        4,
+        6,
+    )
 
 
 def check_tag_sentiment(tid):
-    if tid in (1,2,):
+    if tid in (
+            1,
+            2,
+    ):
         return 1
-    elif tid in (3,4,):
+    elif tid in (
+            3,
+            4,
+    ):
         return 0
-    elif tid in (5,6,):
+    elif tid in (
+            5,
+            6,
+    ):
         return -1
     assert False, 'illegal tid {}'.format(tid)
 
@@ -36,13 +52,97 @@ def is_int(s):
         return False
 
 
+def gen_initial_data(path, tokenizer, texsmart_path, out_path, out_question_path):
+    texsmart_path_lib = os.path.join(texsmart_path, "lib/")
+    texsmart_path_kg = os.path.join(texsmart_path, "data/nlu/kb/")
+    if not os.path.isdir(texsmart_path_lib) or not os.path.isdir(texsmart_path_kg):
+        return
+
+    sys.path.append(texsmart_path_lib)
+    from tencent_ai_texsmart import NluEngine
+    texsmart = NluEngine(texsmart_path_kg, 1)
+    opts = {
+        "input_spec": {
+            "lang": "auto"
+        },
+        "word_seg": {
+            "enable": False
+        },
+        "pos_tagging": {
+            "enable": False,
+            "alg": "log_linear"
+        },
+        "ner": {
+            "enable": True,
+            "alg": "fine.std"
+        },
+        "syntactic_parsing": {
+            "enable": False
+        },
+        "srl": {
+            "enable": False
+        }
+    }
+    opts_str = json.dumps(opts)
+    print("Loading Texsmart succeed!")
+
+    data = load_data(path, tokenizer)
+
+    new_data = []
+    new_data_questions = []
+    for instance in data:
+        conv = [tokenizer.convert_ids_to_tokens(x) for x in instance["conv"]]
+        for i in range(len(conv) - 1):
+            conv_str = ''.join(conv[i][1:])
+            # conv_entities = texsmart.parse_text_ext(conv_str, opts_str).entities()
+            if conv[i][-1] in (
+                    '?',
+                    '？') and '他' not in conv_str and '她' not in conv_str and '它' not in conv_str and '喜欢' in conv_str:
+                turn1 = ''.join(conv[i][1:])
+                turn2 = ''.join(conv[i + 1][1:])
+                new_data_questions.append({'conv': [turn1, turn2]})
+        for senti in instance["sentiment"]:
+            for mentn in instance["mention"]:
+                if mentn["var"] in senti["variables"] and (mentn["turn_id"] + 1 == senti["turn_id"] or
+                                                           mentn["turn_id"] == senti["turn_id"]):
+
+                    mentn_str = ''.join(conv[mentn["turn_id"]][mentn['span'][0]:mentn['span'][1] + 1])
+                    mentn_entities = texsmart.parse_text_ext(mentn_str, opts_str).entities()
+                    if len(mentn_entities) == 0:
+                        continue
+
+                    new_conv = []
+                    for tid in range(mentn["turn_id"], senti["turn_id"] + 1):
+                        new_turn = ' '.join(conv[tid][1:])
+                        new_conv.append(new_turn)
+
+                    new_senti = copy.copy(senti)
+                    new_senti['turn_id'] = len(new_conv) - 1  # alsways the last turn
+                    new_senti['span'] = [x - 1 for x in senti['span']]  # adjust as 'A:/B:' is removed
+
+                    new_mentn = copy.copy(mentn)
+                    new_mentn['turn_id'] = 0  # always the first turn
+                    new_mentn['span'] = [x - 1 for x in mentn['span']]  # adjust as 'A:/B:' is removed
+                    new_mentn['category'] = mentn_entities[0].type.i18n
+
+                    new_data.append({'conv': new_conv, 'sentiment': new_senti, 'mention': new_mentn})
+
+    #with open(out_path, "w") as f:
+    #    for x in new_data:
+    #        f.write(json.dumps(x, ensure_ascii=False) + '\n')
+
+    with open(out_question_path, "w") as f:
+        for x in new_data_questions:
+            f.write('\n'.join(x["conv"]) + '\n\n')
+
+
 def load_data(path, tokenizer):
     data = []
     conv, sentiment, mention = [], [], []
     for i, line in enumerate(open(path, 'r')):
-        if line.strip() == '': # end of a dialogue
+        if line.strip() == '':  # end of a dialogue
             if len(conv) > 0:
-                data.append({'conv':conv, 'sentiment':sentiment, 'mention':mention})
+                data.append({'conv': conv, 'sentiment': sentiment, 'mention': mention})
                 #for x in data[-1]['mention']:
                 #    tid = x['turn_id']
                 #    st, ed = x['span']
@@ -59,20 +159,25 @@ def load_data(path, tokenizer):
                     flag = True
                 elif tok.endswith(']') and (tok == ']' or is_int(tok[:-1])):
                     assert flag == True, 'Erroneous CASA annotations for {}'.format(line)
-                    ed = len(turn) - 1 # [st, ed]
+                    ed = len(turn) - 1  # [st, ed]
                     senti = None if tok == ']' else int(tok[:-1])
-                    if senti != None: # sentiment
+                    if senti != None:  # sentiment
                         assert senti in (-1, 0, 1)
-                        sentiment.append({'variables':variables, 'span':(st,ed), 'turn_id':len(conv), 'senti':senti})
-                    else: # mention
+                        sentiment.append({
+                            'variables': variables,
+                            'span': (st, ed),
+                            'turn_id': len(conv),
+                            'senti': senti
+                        })
+                    else:  # mention
                         assert len(variables) == 1, 'Erroneous CASA annotations for {}'.format(line)
-                        mention.append({'var':variables[0], 'span':(st,ed), 'turn_id':len(conv)})
+                        mention.append({'var': variables[0], 'span': (st, ed), 'turn_id': len(conv)})
                     flag = False
                 else:
                     turn.extend(tokenizer.encode(tok, add_special_tokens=False))
             conv.append(turn)
     if len(conv) > 0:
-        data.append({'conv':conv, 'sentiment':sentiment, 'mention':mention})
+        data.append({'conv': conv, 'sentiment': sentiment, 'mention': mention})
         #for x in data[-1]['mention']:
         #    tid = x['turn_id']
         #    st, ed = x['span']
@@ -100,9 +205,11 @@ def load_and_extract_features(path, tokenizer, task, portion="all"):
         num_mntn += len(mention)
         num_mntn_tokens += sum([x['span'][1] - x['span'][0] + 1 for x in mention])
     print('Number of convs {} and sentences {}'.format(num_conv, num_sentence))
-    print('Number of sentiments {}, cross {}, pos {}, neu {}, neg {}'.format(num_senti,
-        num_senti_cross/num_senti, num_senti_pos/num_senti, num_senti_neu/num_senti, num_senti_neg/num_senti))
-    print('Number of mention {}, avg tokens {:.2f}'.format(num_mntn, num_mntn_tokens/num_mntn))
+    print('Number of sentiments {}, cross {}, pos {}, neu {}, neg {}'.format(num_senti, num_senti_cross / num_senti,
+                                                                             num_senti_pos / num_senti,
+                                                                             num_senti_neu / num_senti,
+                                                                             num_senti_neg / num_senti))
+    print('Number of mention {}, avg tokens {:.2f}'.format(num_mntn, num_mntn_tokens / num_mntn))
 
     if task == 'sentiment':
         return extract_features_sentiment(data, tokenizer)
@@ -118,7 +225,7 @@ def extract_features_sentiment(data, tokenizer):
     #unk_count, total_count = 0.0, 0.0
     for dialogue in data:
         for i, input_ids in enumerate(dialogue['conv']):
-            input_tags = [TAG_MAPPING['O'] for _ in input_ids] # [seq]
+            input_tags = [TAG_MAPPING['O'] for _ in input_ids]  # [seq]
             #unk_count += sum([x == UNK_ID for x in input_ids])
             #total_count += len(input_ids)
             refs = set()
@@ -127,10 +234,10 @@ def extract_features_sentiment(data, tokenizer):
                     st, ed = senti['span']
                     refs.add((st, ed, senti['senti']))
                     senti_str = SENTI_STR_MAPPING[senti['senti']]
-                    input_tags[st] = TAG_MAPPING[senti_str+'B']
-                    for j in range(st+1, ed+1):
-                        input_tags[j] = TAG_MAPPING[senti_str+'I']
-            features.append({'input_ids':input_ids, 'input_tags':input_tags, 'refs':refs})
+                    input_tags[st] = TAG_MAPPING[senti_str + 'B']
+                    for j in range(st + 1, ed + 1):
+                        input_tags[j] = TAG_MAPPING[senti_str + 'I']
+            features.append({'input_ids': input_ids, 'input_tags': input_tags, 'refs': refs})
     #print('UNK rate: {:.2f}'.format(100*unk_count/total_count))
     return features
 
@@ -141,12 +248,14 @@ def extract_features_mention(data, tokenizer, portion):
     features = []
     for dialogue in data:
         all_ids = []
-        all_offsets = [0,]
-        all_sentids = [] # start from 1 to avoid padding 0s
+        all_offsets = [
+            0,
+        ]
+        all_sentids = []  # start from 1 to avoid padding 0s
         for i, cur_ids in enumerate(dialogue['conv']):
             all_ids += cur_ids
-            all_offsets.append(all_offsets[-1]+len(cur_ids))
-            all_sentids.extend([i+1 for _ in cur_ids])
+            all_offsets.append(all_offsets[-1] + len(cur_ids))
+            all_sentids.extend([i + 1 for _ in cur_ids])
             for senti in dialogue['sentiment']:
                 if portion == 'cross' and senti['is_cross'] == False:
                     continue
@@ -157,16 +266,20 @@ def extract_features_mention(data, tokenizer, portion):
                     has_mention = False
 
                     # ADD w_1^1, ..., w_1^{N_1}, ..., w_i^{N_i} [SEP]
-                    input_ids = all_ids + [SEP_ID,]
-                    input_sentids = all_sentids + [i+2,]
+                    input_ids = all_ids + [
+                        SEP_ID,
+                    ]
+                    input_sentids = all_sentids + [
+                        i + 2,
+                    ]
                     input_senti_mask = [0.0 for _ in input_ids]
-                    input_content_bound = len(input_ids)-1
+                    input_content_bound = len(input_ids) - 1
 
                     # ADD w_{s_j}^1, ..., w_{s_j}^{|s_j|}
-                    senti_st, senti_ed = senti['span'] # [st, ed]
-                    senti_ids = cur_ids[senti_st:senti_ed+1]
+                    senti_st, senti_ed = senti['span']  # [st, ed]
+                    senti_ids = cur_ids[senti_st:senti_ed + 1]
                     input_ids += senti_ids
-                    input_sentids.extend([i+3 for _ in senti_ids])
+                    input_sentids.extend([i + 3 for _ in senti_ids])
                     input_senti_mask.extend([1.0 for _ in senti_ids])
                     #print(tokenizer.decode(senti_ids))
 
@@ -179,13 +292,20 @@ def extract_features_mention(data, tokenizer, portion):
                             #tid = mentn['turn_id']
                             #print('\t{}'.format(tokenizer.decode(dialogue['conv'][tid][st:ed+1])))
                             st, ed = st + all_offsets[mentn['turn_id']], ed + all_offsets[mentn['turn_id']]
-                            input_ref.append((st,ed))
+                            input_ref.append((st, ed))
                             #print('\t{}'.format(tokenizer.decode(input_ids[st:ed+1])))
-                            refs.add(tokenizer.decode(input_ids[st:ed+1]))
+                            refs.add(tokenizer.decode(input_ids[st:ed + 1]))
                     if has_mention:
-                        features.append({'input_ids':input_ids, 'input_sentids':input_sentids, 'input_ref':input_ref,
-                            'input_senti_mask':input_senti_mask, 'input_content_bound':input_content_bound, 'refs':refs,
-                            'is_cross':senti['is_cross'], 'senti':senti['senti']})
+                        features.append({
+                            'input_ids': input_ids,
+                            'input_sentids': input_sentids,
+                            'input_ref': input_ref,
+                            'input_senti_mask': input_senti_mask,
+                            'input_content_bound': input_content_bound,
+                            'refs': refs,
+                            'is_cross': senti['is_cross'],
+                            'senti': senti['senti']
+                        })
     return features
 
 
@@ -201,20 +321,22 @@ def make_batch(features, task, batch_size, is_sort=True, is_shuffle=False):
 def make_batch_unified(features, B, N):
     maxseq = 0
     for i in range(0, B):
-        maxseq = max(maxseq, len(features[N+i]['input_ids']))
+        maxseq = max(maxseq, len(features[N + i]['input_ids']))
 
     input_ids = np.zeros([B, maxseq], dtype=np.long)
     input_mask = np.zeros([B, maxseq], dtype=np.float)
 
     for i in range(0, B):
-        curseq = len(features[N+i]['input_ids'])
-        input_ids[i,:curseq] = features[N+i]['input_ids']
-        input_mask[i,:curseq] = [1,]*curseq
+        curseq = len(features[N + i]['input_ids'])
+        input_ids[i, :curseq] = features[N + i]['input_ids']
+        input_mask[i, :curseq] = [
+            1,
+        ] * curseq
 
     input_ids = torch.tensor(input_ids, dtype=torch.long)
     input_mask = torch.tensor(input_mask, dtype=torch.float)
 
-    return {'input_ids':input_ids, 'input_mask':input_mask}, maxseq
+    return {'input_ids': input_ids, 'input_mask': input_mask}, maxseq
 
 
 def make_batch_sentiment(features, batch_size, is_sort=True, is_shuffle=False):
@@ -225,15 +347,15 @@ def make_batch_sentiment(features, batch_size, is_sort=True, is_shuffle=False):
     N = 0
     batches = []
     while N < len(features):
-        B = min(batch_size, len(features)-N)
+        B = min(batch_size, len(features) - N)
         batch, maxseq = make_batch_unified(features, B, N)
         if features[N]['input_tags'] != None:
             input_tags = np.zeros([B, maxseq], dtype=np.long)
             for i in range(0, B):
-                curseq = len(features[N+i]['input_tags'])
-                input_tags[i,:curseq] = features[N+i]['input_tags']
+                curseq = len(features[N + i]['input_tags'])
+                input_tags[i, :curseq] = features[N + i]['input_tags']
             batch['input_tags'] = torch.tensor(input_tags, dtype=torch.long)
-            batch['refs'] = [features[N+i]['refs'] for i in range(0, B)]
+            batch['refs'] = [features[N + i]['refs'] for i in range(0, B)]
         else:
             batch['input_tags'] = None
             batch['refs'] = None
@@ -250,32 +372,34 @@ def make_batch_mention(features, batch_size, is_sort=True, is_shuffle=False):
     N = 0
     batches = []
     while N < len(features):
-        B = min(batch_size, len(features)-N)
+        B = min(batch_size, len(features) - N)
         batch, maxseq = make_batch_unified(features, B, N)
         input_sentids = np.zeros([B, maxseq], dtype=np.float)
         input_senti_mask = np.zeros([B, maxseq], dtype=np.float)
         input_content_mask = np.zeros([B, maxseq], dtype=np.float)
         input_ref = np.zeros([B, maxseq, 2], dtype=np.float)
         for i in range(0, B):
-            curseq = len(features[N+i]['input_ids'])
-            input_sentids[i,:curseq] = features[N+i]['input_sentids']
-            input_senti_mask[i,:curseq] = features[N+i]['input_senti_mask']
-            bound = features[N+i]['input_content_bound']
-            input_content_mask[i,:bound] = [1.0,] * bound
+            curseq = len(features[N + i]['input_ids'])
+            input_sentids[i, :curseq] = features[N + i]['input_sentids']
+            input_senti_mask[i, :curseq] = features[N + i]['input_senti_mask']
+            bound = features[N + i]['input_content_bound']
+            input_content_mask[i, :bound] = [
+                1.0,
+            ] * bound
             if features[N]['input_ref'] != None:
-                ref_num = len(features[N+i]['input_ref'])
+                ref_num = len(features[N + i]['input_ref'])
                 assert ref_num > 0
-                for st, ed in features[N+i]['input_ref']:
-                    input_ref[i,st,0] = 1.0/ref_num
-                    input_ref[i,ed,1] = 1.0/ref_num
+                for st, ed in features[N + i]['input_ref']:
+                    input_ref[i, st, 0] = 1.0 / ref_num
+                    input_ref[i, ed, 1] = 1.0 / ref_num
         batch['input_sentids'] = torch.tensor(input_sentids, dtype=torch.float)
         batch['input_senti_mask'] = torch.tensor(input_senti_mask, dtype=torch.float)
         batch['input_content_mask'] = torch.tensor(input_content_mask, dtype=torch.float)
-        batch['senti'] = [features[N+i]['senti'] for i in range(0, B)]
+        batch['senti'] = [features[N + i]['senti'] for i in range(0, B)]
         if features[N]['input_ref'] != None:
             batch['input_ref'] = torch.tensor(input_ref, dtype=torch.float)
-            batch['refs'] = [features[N+i]['refs'] for i in range(0, B)]
-            batch['is_cross'] = [features[N+i]['is_cross'] for i in range(0, B)]
+            batch['refs'] = [features[N + i]['refs'] for i in range(0, B)]
+            batch['is_cross'] = [features[N + i]['is_cross'] for i in range(0, B)]
         else:
             batch['input_ref'] = None
             batch['refs'] = None
@@ -285,4 +409,10 @@ def make_batch_mention(features, batch_size, is_sort=True, is_shuffle=False):
     return batches
 
 
-
+if __name__ == '__main__':
+    SPECIAL_TOKENS = {'additional_special_tokens': ['[filler0]', 'A:', 'B:', '[filler1]', '[filler2]']}
+    tokenizer = BertTokenizer.from_pretrained("hfl/chinese-bert-wwm-ext")
+    tokenizer.add_special_tokens(SPECIAL_TOKENS)
+    texsmart_path = "/harddisk/user/lfsong/services/texsmart-sdk-0.3.6-s/"
+    gen_initial_data("data/duconv.txt", tokenizer, texsmart_path, "data/initial_train_data.jsonl",
+                     "data/initial_question_data.txt")
--- asa_model.py
+++ asa_model.py
@@ -1,49 +1,46 @@
-
 import torch
 import torch.nn as nn
-import os, sys, json, codecs
 from transformers import BertModel
-from nn_utils import AdditiveAttention, gather_tok2word, has_nan
+from nn_utils import AdditiveAttention, has_nan
 from asa_datastream import TAG_MAPPING
 
 
 class BertAsaSe(nn.Module):
+
     def __init__(self, path):
         super(BertAsaSe, self).__init__()
         self.embed = None
         self.bert = BertModel.from_pretrained(path)
         self.dropout = nn.Dropout(0.1)
         self.label_num = len(TAG_MAPPING)
-        self.classifier = nn.Sequential(torch.nn.Linear(self.bert.config.hidden_size, 384),
-                nn.ReLU(), nn.Linear(384, self.label_num))
-
+        self.classifier = nn.Sequential(torch.nn.Linear(self.bert.config.hidden_size, 384), nn.ReLU(),
+                                        nn.Linear(384, self.label_num))
 
     def freeze_bert(self):
         for param in self.bert.parameters():
             param.requires_grad = False
 
-
     def setup_embedding(self, vocab_size):
         self.embed = nn.Embedding(vocab_size, 768)
-
 
     def forward(self, batch):
         # embedding
         if self.embed is None:
-            tok_repre = self.bert(input_ids=batch['input_ids'], attention_mask=batch['input_mask'], return_dict=True).last_hidden_state
-            tok_repre = self.dropout(tok_repre) # [batch, seq, dim]
+            tok_repre = self.bert(input_ids=batch['input_ids'], attention_mask=batch['input_mask'],
+                                  return_dict=True).last_hidden_state
+            tok_repre = self.dropout(tok_repre)  # [batch, seq, dim]
         else:
-            tok_repre = self.embed(batch['input_ids']) # [batch, seq, dim]
+            tok_repre = self.embed(batch['input_ids'])  # [batch, seq, dim]
 
         batch_size, seq_len, _ = list(tok_repre.size())
         total_len = batch_size * seq_len
 
         # make predictions
-        logits = self.classifier(tok_repre).log_softmax(dim=2) # [batch, seq, label]
-        predictions = logits.argmax(dim=2) # [batch, seq]
+        logits = self.classifier(tok_repre).log_softmax(dim=2)  # [batch, seq, label]
+        predictions = logits.argmax(dim=2)  # [batch, seq]
 
         if batch['input_tags'] is not None:
-            active_positions = (batch['input_mask'] == 1.0).view(total_len) # [batch * seq]
+            active_positions = (batch['input_mask'] == 1.0).view(total_len)  # [batch * seq]
             active_logits = logits.view(total_len, self.label_num)[active_positions]
             active_refs = batch['input_tags'].view(total_len)[active_positions]
             loss = nn.CrossEntropyLoss()(active_logits, active_refs)
@@ -51,10 +48,11 @@ class BertAsaSe(nn.Module):
             loss = torch.tensor(0.0).cuda() if torch.cuda.is_available() else torch.tensor(0.0)
 
         seq_lengths = batch['input_mask'].sum(dim=1).long()
-        return {'loss':loss, 'predictions':predictions, 'seq_lengths':seq_lengths}
+        return {'loss': loss, 'predictions': predictions, 'seq_lengths': seq_lengths}
 
 
 class BertAsaMe(nn.Module):
+
     def __init__(self, path):
         super(BertAsaMe, self).__init__()
         self.embed = None
@@ -64,15 +62,12 @@ class BertAsaMe(nn.Module):
         self.st_classifier = AdditiveAttention(hidden_size, hidden_size, 384)
         self.ed_classifier = AdditiveAttention(hidden_size, hidden_size, 384)
 
-
     def freeze_bert(self):
         for param in self.bert.parameters():
             param.requires_grad = False
 
-
     def setup_embedding(self, vocab_size):
         self.embed = nn.Embedding(vocab_size, 768)
-
 
     def get_bert_embedding(self, batch):
         tokseq_num = batch['input_ids'].shape[1]
@@ -80,32 +75,32 @@ class BertAsaMe(nn.Module):
             tok_repre = []
             st = 0
             while st < tokseq_num:
-                ed = min(tokseq_num, st+512)
-                cur_ids = batch['input_ids'][:,st:ed]
-                cur_mask = batch['input_mask'][:,st:ed]
+                ed = min(tokseq_num, st + 512)
+                cur_ids = batch['input_ids'][:, st:ed]
+                cur_mask = batch['input_mask'][:, st:ed]
                 cur_tok_repre, _ = self.bert(input_ids=cur_ids, attention_mask=cur_mask, return_dict=True)
                 tok_repre.append(cur_tok_repre.last_hidden_state)
                 st = ed
             tok_repre = torch.cat(tok_repre, dim=1)
         else:
-            tok_repre = self.bert(input_ids=batch['input_ids'], attention_mask=batch['input_mask'], return_dict=True).last_hidden_state
+            tok_repre = self.bert(input_ids=batch['input_ids'], attention_mask=batch['input_mask'],
+                                  return_dict=True).last_hidden_state
         return tok_repre
-
 
     def forward(self, batch):
         # embedding
         if self.embed == None:
-            tok_repre = self.get_bert_embedding(batch) # [batch, seq, dim]
-            tok_repre = self.dropout(tok_repre) # [batch, seq, dim]
+            tok_repre = self.get_bert_embedding(batch)  # [batch, seq, dim]
+            tok_repre = self.dropout(tok_repre)  # [batch, seq, dim]
         else:
             tok_repre = self.embed(batch['input_ids'])
 
         # generate final distribution
-        senti_repre = (tok_repre * batch['input_senti_mask'].unsqueeze(-1)).sum(dim=1) # [batch, dim]
-        _, st_dist = self.st_classifier(senti_repre, tok_repre, batch['input_content_mask']) # [batch, seq]
-        _, ed_dist = self.ed_classifier(senti_repre, tok_repre, batch['input_content_mask']) # [batch, seq]
+        senti_repre = (tok_repre * batch['input_senti_mask'].unsqueeze(-1)).sum(dim=1)  # [batch, dim]
+        _, st_dist = self.st_classifier(senti_repre, tok_repre, batch['input_content_mask'])  # [batch, seq]
+        _, ed_dist = self.ed_classifier(senti_repre, tok_repre, batch['input_content_mask'])  # [batch, seq]
         assert has_nan(st_dist.log()) == False and has_nan(ed_dist.log()) == False
-        final_dist = st_dist.unsqueeze(dim=2) * ed_dist.unsqueeze(dim=1) # [batch, wordseq, wordseq]
+        final_dist = st_dist.unsqueeze(dim=2) * ed_dist.unsqueeze(dim=1)  # [batch, wordseq, wordseq]
 
         # make predictions
         batch_size, seq_num, bert_dim = list(tok_repre.size())
@@ -114,26 +109,24 @@ class BertAsaMe(nn.Module):
         if torch.cuda.is_available():
             a = a.cuda()
             b = b.cuda()
-        mask = (a <= b).float() # [batch, seq, seq]
+        mask = (a <= b).float()  # [batch, seq, seq]
         span_mask = (torch.abs(a - b) <= 6).float()
         sentid = batch['input_sentids']
-        sentid_mask = (sentid.unsqueeze(dim=1) == sentid.unsqueeze(dim=2)).float() # [batch, seq, seq]
+        sentid_mask = (sentid.unsqueeze(dim=1) == sentid.unsqueeze(dim=2)).float()  # [batch, seq, seq]
         predictions = (final_dist * mask * span_mask * sentid_mask).view(batch_size,
-                seq_num * seq_num).argmax(dim=1) # [batch]
+                                                                         seq_num * seq_num).argmax(dim=1)  # [batch]
 
         # calculate loss
         if batch['input_ref'] is not None:
-            st_ref, ed_ref = batch['input_ref'].split(1, dim=-1) # [batch, seq, 1]
-            st_ref, ed_ref = st_ref.squeeze(dim=-1), ed_ref.squeeze(dim=-1) # [batch, seq]
+            st_ref, ed_ref = batch['input_ref'].split(1, dim=-1)  # [batch, seq, 1]
+            st_ref, ed_ref = st_ref.squeeze(dim=-1), ed_ref.squeeze(dim=-1)  # [batch, seq]
             assert has_nan(st_ref) == False and has_nan(ed_ref) == False
             tmp_st = st_ref * (st_dist.log())
             tmp_ed = ed_ref * (ed_dist.log())
             assert has_nan(tmp_st) == False and has_nan(tmp_ed) == False
-            tmp = (tmp_st + tmp_ed) * batch['input_content_mask'] # [batch, seq]
+            tmp = (tmp_st + tmp_ed) * batch['input_content_mask']  # [batch, seq]
             loss = -1.0 * tmp.sum() / batch['input_content_mask'].sum()
         else:
             loss = torch.tensor(0.0).cuda() if torch.cuda.is_available() else torch.tensor(0.0)
 
         return {'loss': loss, 'predictions': predictions, 'seq_num': seq_num}
-
-
